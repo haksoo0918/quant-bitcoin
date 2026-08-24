afbf1720365791bae8d61c6384e68f95224558a2
haksoo <haksoo0918@gmail.com>
2026-08-24 19:23:27 +0900

    feat: 웹 대시보드 알트코인 한글명 병기, 서브전략 제목 빗썸 명시 및 상태바 문구 개선 (v1.5.8)

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for Quant Crypto Dashboard PWA
-const CACHE_NAME = 'quant-dashboard-v1.5.6';
+const CACHE_NAME = 'quant-dashboard-v1.5.8';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
